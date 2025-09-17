c5d26e801847c188a5dcb6602a1999c8fcab834a
Ketki Mohite <ketkimohite214@gmail.com>
2025-09-17 20:57:47 +0530

    Day-8 Personalized Greeting

---
+++ personalized-greeting/greeting.py
@@ -0,0 +1,15 @@
+#welcome the user
+print("Simple Greeting Generator")
+
+#ask for users name and storing it in a variable
+your_name = input("What is your name?")
+
+#ask for the friends name and store it in another variable
+friends_name = input("What is your friend's name?")
+
+#create and print the greeting
+print("\n----------")
+print(f"Hello,{friends_name}!")
+print(f"I hope you are having a wonderful day")
+print(f"From your friend,{your_name}")
+print("----------")
