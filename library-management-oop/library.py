e41af310a97eab209ae149d856a9efb559477fd2
Ketki Mohite <ketkimohite214@gmail.com>
2025-09-26 00:13:32 +0530

    This is it

--- library-management-oop/library.py
+++ library-management-oop/library.py
@@ -44,7 +44,6 @@ class Member(User):
         else:
             print(f"{self.firstname} {self.lastname} does not have this book borrowed.")
 
-# Example usage with Indian names:
 book1 = Book("1984", "George Orwell", 1949)
 book2 = Book("To Kill a Mockingbird", "Harper Lee", 1960)
 
