2338f6c29d214b84509de5480c938bc8bfbb23c5
Ketki Mohite <ketkimohite214@gmail.com>
2025-10-10 17:08:03 +0530

    Day-31 Starting Geeks for Geeks

---
+++ geeksforgeeks/day-31-basics.py
@@ -0,0 +1,31 @@
+print("Hello World")
+#Comment in python 
+#python variables are case sensitive 
+# Variable 'x' stores the integer value 10
+x = 5
+
+# Variable 'name' stores the string "Samantha"
+name = "Samantha"  
+
+print(x)
+print(name)
+
+x = 5
+y = 3.14
+z = "Hi"
+
+# Define variables with different data types
+n = 42
+f = 3.14
+s = "Hello, World!"
+li = [1, 2, 3]
+d = {'key': 'value'}
+bool = True
+
+# Get and print the type of each variable
+print(type(n))   
+print(type(f)) 
+print(type(s))   
+print(type(li))     
+print(type(d))     
+print(type(bool))
